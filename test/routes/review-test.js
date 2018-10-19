76eddc317478ea714713cfd286e14ede161d8589
William Wall <william@williamwall.ie>
2018-10-19 11:29:01 +0100

    Created nested describe block/it template for review-test tests

---
+++ test/routes/review-test.js
@@ -0,0 +1,41 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let expect = chai.expect;
+
+chai.use(chaiHttp);
+let _ = require('lodash');
+chai.use(require('chai-things'));
+
+describe('Reviews', function () {
+
+    describe('GET /reviews', () => {
+        it('', function () {
+
+        });
+    });
+    describe('POST /reviews', function () {
+        it('', function () {
+
+        });
+    });
+    describe('PUT /reviews/:id/', () => {
+        it('', function () {
+
+        });
+        it('', function () {
+
+        });
+    });
+});
+describe('DELETE /reviews/:id', () => {
+    describe('', function () {
+        it('', function () {
+
+        });
+    });
+    describe('', function () {
+        it('', function () {
+
+        });
+    });
+});
