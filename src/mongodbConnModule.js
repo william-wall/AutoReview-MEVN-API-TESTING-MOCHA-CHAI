2359792d2f610f0619c10cf40b55b3c8e5c18563
William Wall <william@williamwall.ie>
2018-10-19 10:03:02 +0100

    Switching from mongo localhost to cloud hosted database

--- src/mongodbConnModule.js
+++ src/mongodbConnModule.js
@@ -2,9 +2,8 @@
 var mongoose = require('mongoose');
 
 module.exports.connect = function() {
-	mongoose.connect('mongodb://localhost:27017/MEVN-boilerplate');
-    // mongodb://will:william1@ds125341.mlab.com:25341/post-app
-    //
+	mongoose.connect('mongodb://will:william1@ds125341.mlab.com:25341/post-app');
+    // mongodb://localhost:27017/MEVN-boilerplate
 	var db = mongoose.connection;
 	db.on("error", console.error.bind(console, "connection error"));
 	db.once("open", function(callback){
