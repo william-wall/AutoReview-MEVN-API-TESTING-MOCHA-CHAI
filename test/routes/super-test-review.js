e34842f7b2c1cbf96fb4271b5a30abb5ebe2b20a
William Wall <william@williamwall.ie>
2018-10-28 20:16:15 +0000

    supertest find by single id test and update by id test

--- test/routes/super-test-review.js
+++ test/routes/super-test-review.js
@@ -17,19 +17,27 @@ describe("SuperTest Reviews", function () {
         ' between April 2014 and December 2017.'
     };
 
-    describe('Get Reviews', function() {
+    describe('Get Reviews', function () {
 
         it("should return reviews", function (done) {
-        server
-            .get("/reviews")
-            .expect("Content-type", /json/)
-            .expect(200)
-            .end(function (err, res) {
-                res.status.should.equal(200);
-                console.log(res.body.reviews);
-                done();
-            });
-    });
+            server
+                .get('/reviews')
+                .expect('Content-type', /json/)
+                .expect(200)
+                .end(function (err, res) {
+                    res.status.should.equal(200);
+                    // console.log(res.body.reviews);
+                    let result = _.map(res.body.reviews, (reviews) => {
+                        return {
+                            _id: reviews._id,
+                            title: reviews.title,
+                            description: reviews.description
+                        }
+                    });
+                    console.log(result);
+                    done();
+                });
+        });
     });
 
 
@@ -46,6 +54,43 @@ describe("SuperTest Reviews", function () {
         });
     });
 
+
+
+
+
+    describe('Get a review by id', function() {
+
+        it('should get a review by its id', function(done) {
+            server
+                .get('/reviews/' + review._id)
+                .expect("Content-type", /json/)
+                .expect(200)
+                .end(function (err, res) {
+                    res.status.should.equal(200);
+                    done();
+                });
+        });
+    });
+
+
+    describe('Update a review by id', function() {
+        it('should modify a review', function(done) {
+
+        review.title = 'updated title', review.description = 'updated description'
+            server
+                .get('/reviews')
+                .end(function (err, res) {
+                    server
+                        .put('/reviews/'+ res.body.reviews[0]._id)
+                        .send(review)
+                        .end(function(err, res) {
+                            expect(res.statusCode).to.equal(200);
+                            done();
+                        });
+                });
+
+        });
+    });
 
 });
 
