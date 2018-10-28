972ad852aae7d3ae4234de90522c9c9fab61590f
William Wall <william@williamwall.ie>
2018-10-28 12:42:16 +0000

    Heroku/mLab post test supertest

--- test/routes/super-test-review.js
+++ test/routes/super-test-review.js
@@ -1,25 +1,51 @@
 var supertest = require("supertest");
 var should = require("should");
+var chai = require('chai');
+var expect = chai.expect;
 
 let _ = require('lodash');
 
 var server = supertest.agent("http://autoreview.herokuapp.com");
 
 
-describe("SAMPLE unit test", function () {
+describe("SuperTest Reviews", function () {
 
-    it("should return home page", function (done) {
+    var review = {
+        title: 'Citroën C4 Cactus 2018',
+        description: 'The Citroën C4 Cactus is a subcompact crossover SUV,' +
+        ' produced by French automaker Citroën in Spain' +
+        ' between April 2014 and December 2017.'
+    };
 
+    describe('Get Reviews', function() {
+
+        it("should return reviews", function (done) {
         server
             .get("/reviews")
             .expect("Content-type", /json/)
-            .expect(200) // THis is HTTP response
+            .expect(200)
             .end(function (err, res) {
                 res.status.should.equal(200);
-                console.log(res.body);
+                console.log(res.body.reviews);
                 done();
             });
     });
+    });
+
+
+    describe('Post Review ', function() {
+        it('should create a review', function(done) {
+                server
+                    .post('/reviews')
+                .send(review)
+                .end(function(err, res) {
+                    expect(res.statusCode).to.equal(200);
+                    review = res.body;
+                    done();
+                });
+        });
+    });
+
 
 });
 
