24c60f42e139b0a4dce44edb209adce0a1dcf813
William Wall <william@williamwall.ie>
2018-11-05 20:42:21 +0000

    find by id test-supertest, and additional expectations for tests

--- test/routes/super-test-review.js
+++ test/routes/super-test-review.js
@@ -5,17 +5,44 @@ var expect = chai.expect;
 
 let _ = require('lodash');
 
-var server = supertest.agent("http://autoreview.herokuapp.com");
+var server = supertest.agent("http://autoreview-testing.herokuapp.com");
 
 
 describe("SuperTest Reviews", function () {
 
     var review = {
         title: 'Citroën C4 Cactus 2018',
-        description: 'The Citroën C4 Cactus is a subcompact crossover SUV,' +
-        ' produced by French automaker Citroën in Spain' +
-        ' between April 2014 and December 2017.'
+        description: 'The Citroën C4 Cactus is a subcompact crossover SUV'
     };
+
+    describe('Post Review ', function () {
+
+        it('should create a review', function (done) {
+
+            server
+                .post('/reviews')
+                .send(review)
+                .end(function (err, res) {
+                    server
+                        .get('/reviews')
+                        .end(function (err, res) {
+                            expect(res.statusCode).to.equal(200);
+                            review = res.body;
+                            let result = _.map(res.body.reviews, (reviews) => {
+                                return {
+                                    title: reviews.title,
+                                    description: reviews.description
+                                }
+                            });
+                            expect(result).to.include({
+                                title: 'Citroën C4 Cactus 2018',
+                                description: 'The Citroën C4 Cactus is a subcompact crossover SUV'
+                            });
+                            done();
+                        });
+                });
+        });
+    });
 
     describe('Get Reviews', function () {
 
@@ -26,30 +53,38 @@ describe("SuperTest Reviews", function () {
                 .expect(200)
                 .end(function (err, res) {
                     res.status.should.equal(200);
-                    // console.log(res.body.reviews);
                     let result = _.map(res.body.reviews, (reviews) => {
                         return {
-                            _id: reviews._id,
                             title: reviews.title,
                             description: reviews.description
                         }
+                    });
+                    expect(result).to.include({
+                        title: 'Citroën C4 Cactus 2018',
+                        description: 'The Citroën C4 Cactus is a subcompact crossover SUV'
                     });
                     done();
                 });
         });
 
-    });
-
-
-    describe('Post Review ', function () {
-        it('should create a review', function (done) {
+        it('should return a single object by its id', function (done) {
+            var singleReview = {_id: '0000', title: 'single review title', description: 'single review description'};
             server
                 .post('/reviews')
-                .send(review)
+                .send(singleReview)
                 .end(function (err, res) {
-                    expect(res.statusCode).to.equal(200);
-                    review = res.body;
-                    done();
+                    server
+                        .get('/reviews/' + singleReview._id)
+                        .expect(200)
+                        .end((err, res) => {
+                            expect(singleReview).to.be.a('object');
+                            expect(singleReview).to.include({
+                                _id: '0000',
+                                title: 'single review title',
+                                description: 'single review description'
+                            });
+                            done();
+                        });
                 });
         });
     });
