a56b5be606cafe5625863cabf0d6c6eae28cedd8
William Wall <william@williamwall.ie>
2018-11-06 10:18:57 +0000

    nested describes for super-test-review

--- test/routes/super-test-review.js
+++ test/routes/super-test-review.js
@@ -21,7 +21,7 @@ describe("SuperTest Reviews", function () {
 
     describe('Post Review ', function () {
 
-        it('should create a review', function (done) {
+        it('should create a review and verify its added to the database', function (done) {
 
             server
                 .post('/reviews')
@@ -51,69 +51,76 @@ describe("SuperTest Reviews", function () {
                 });
         });
         it('should create a review', function (done) {
-
             server
                 .post('/reviews')
                 .send(deleteReview)
                 .expect(200)
                 .expect('Content-type', /json/)
                 .end(function (err, res) {
-                   done();
+                    done();
                 });
         });
     });
 
     describe('Get Reviews', function () {
+        describe('Return all Reviews', () => {
 
-        it("should return reviews", function (done) {
-            server
-                .get('/reviews')
-                .expect('Content-type', /json/)
-                .expect(200)
-                .end(function (err, res) {
-                    res.status.should.equal(200);
-                    let result = _.map(res.body.reviews, (reviews) => {
-                        return {
-                            title: reviews.title,
-                            description: reviews.description
-                        }
-                    });
-                    expect(result).to.include({
-                        title: 'Citroën C4 Cactus 2018',
-                        description: 'The Citroën C4 Cactus is a subcompact crossover SUV'
-                    });
-                    done();
-                });
-        });
-
-        it('should return a single object by its id', function (done) {
-            var singleReview = {_id: '0000', title: 'single review title', description: 'single review description'};
-            server
-                .post('/reviews')
-                .expect('Content-type', /json/)
-                .send(singleReview)
-                .expect(200)
-                .end(function (err, res) {
-                    server
-                        .get('/reviews/' + singleReview._id)
-                        .expect('Content-type', /json/)
-                        .expect(200)
-                        .end((err, res) => {
-                            expect(singleReview).to.be.a('object');
-                            expect(singleReview).to.include({
-                                _id: '0000',
-                                title: 'single review title',
-                                description: 'single review description'
-                            });
-                            done();
+            it("should return all reviews and verify", function (done) {
+                server
+                    .get('/reviews')
+                    .expect('Content-type', /json/)
+                    .expect(200)
+                    .end(function (err, res) {
+                        res.status.should.equal(200);
+                        let result = _.map(res.body.reviews, (reviews) => {
+                            return {
+                                title: reviews.title,
+                                description: reviews.description
+                            }
                         });
-                });
+                        expect(result).to.include({
+                            title: 'Citroën C4 Cactus 2018',
+                            description: 'The Citroën C4 Cactus is a subcompact crossover SUV'
+                        });
+                        done();
+                    });
+            });
+        });
+        describe('Return Single Review by ID', () => {
+
+            it('should return a single review object by its id and verify', function (done) {
+                var singleReview = {
+                    _id: '0000',
+                    title: 'single review title',
+                    description: 'single review description'
+                };
+                server
+                    .post('/reviews')
+                    .expect('Content-type', /json/)
+                    .send(singleReview)
+                    .expect(200)
+                    .end(function (err, res) {
+                        server
+                            .get('/reviews/' + singleReview._id)
+                            .expect('Content-type', /json/)
+                            .expect(200)
+                            .end((err, res) => {
+                                expect(singleReview).to.be.a('object');
+                                expect(singleReview).to.include({
+                                    _id: '0000',
+                                    title: 'single review title',
+                                    description: 'single review description'
+                                });
+                                done();
+                            });
+                    });
+            });
         });
     });
 
     describe('Update a review by id', function () {
 
-        it('should modify a review', function (done) {
+        it('should modify a review and verify its changes', function (done) {
             var updateReview = {_id: '1111', title: 'update review title', description: 'update review description'};
             server
                 .get('/reviews')
@@ -139,14 +146,14 @@ describe("SuperTest Reviews", function () {
     });
 
     describe('Delete a review by id', function () {
-        it('should delete a review', function (done) {
+        it('should delete a review and verify it has been removed', function (done) {
             server
                 .get('/reviews')
                 .expect(200)
                 .expect('Content-type', /json/)
                 .end(function (err, res) {
                     server
-                        .delete('/reviews/' +  res.body.reviews[0]._id)
+                        .delete('/reviews/' + res.body.reviews[0]._id)
                         .end(function (err, res) {
                             expect(res.statusCode).to.equal(200);
                             let result = _.map(res.body.reviews, (reviews) => {
