5ca0597d5edc5751ae1aca9348bd11a120f70e18
William Wall <william@williamwall.ie>
2018-11-06 10:12:33 +0000

    nested describes for room-test

--- test/routes/room-test.js
+++ test/routes/room-test.js
@@ -19,7 +19,7 @@ var RoomSchema = require('mongoose').model('Room').schema
 
 const Room = mongoose.model("Room", RoomSchema);
 
-describe('Reviews', function () {
+describe('Rooms', function () {
 
     let someRoom;
     beforeEach((done) => {
@@ -30,48 +30,56 @@ describe('Reviews', function () {
         someRoom.save().then(() => done());
     });
 
-    describe('GET /rooms', () => {
+    describe('GET /api/rooms', () => {
 
         describe('Getting Rooms', () => {
 
-            it('should return all of the rooms objects', function (done) {
-                let aRoom = new Room({room_name: 'Room 2'});
-                aRoom.save()
-                chai.request(app)
-                    .get('/api/rooms/')
-                    .end((err, res) => {
-                        let result = _.map(res.body, (rooms) => {
-                            return {
-                                room_name: rooms.room_name,
-                            }
+            describe('Return all Rooms', () => {
+
+                it('should return all of the rooms objects', function (done) {
+                    let aRoom = new Room({room_name: 'Room 2'});
+                    aRoom.save()
+                    chai.request(app)
+                        .get('/api/rooms/')
+                        .end((err, res) => {
+                            let result = _.map(res.body, (rooms) => {
+                                return {
+                                    room_name: rooms.room_name,
+                                }
+                            });
+                            expect(res).to.have.status(200);
+                            expect(result).to.be.a('array');
+                            expect(result).to.include({room_name: 'Room 1'});
+                            expect(result).to.include({room_name: 'Room 2'});
+                            done();
                         });
-                        expect(res).to.have.status(200);
-                        expect(result).to.be.a('array');
-                        expect(result).to.include({room_name: 'Room 1'});
-                        expect(result).to.include({room_name: 'Room 2'});
-                        expect(res.body.length).to.equal(3);
-                        done();
-                    });
+                });
             });
-            it('should throw a 500 error for incorrect id', function (done) {
-                chai.request(app)
-                    .get('/api/rooms/12')
-                    .end(function (err, res) {
-                        expect(res).to.have.status(500);
-                        done();
-                    });
+            describe('Error Handling', () => {
+
+                it('should throw a 500 error for incorrect id', function (done) {
+                    chai.request(app)
+                        .get('/api/rooms/12')
+                        .end(function (err, res) {
+                            expect(res).to.have.status(500);
+                            done();
+                        });
+                });
             });
-            it('should return a single object by its id', function (done) {
-                let singleRoom = new Room({ room_name: 'Single Room'});
-                singleRoom.save()
-                chai.request(app)
-                    .get('/api/rooms/' + singleRoom._id)
-                    .end((err, res) => {
-                        expect(res).to.have.status(200);
-                        expect(singleRoom).to.be.a('object');
-                        expect(singleRoom.room_name).to.equal('Single Room');
-                        done();
-                    });
+            describe('Return Single Room by ID', () => {
+
+                it('should return a single object by its id', function (done) {
+                    let singleRoom = new Room({room_name: 'Single Room'});
+                    singleRoom.save()
+                    chai.request(app)
+                        .get('/api/rooms/' + singleRoom._id)
+                        .end((err, res) => {
+                            expect(res).to.have.status(200);
+                            expect(singleRoom).to.be.a('object');
+                            expect(singleRoom.room_name).to.equal('Single Room');
+                            done();
+                        });
+                });
             });
         });
     });
@@ -115,54 +123,59 @@ describe('Reviews', function () {
 
         describe('Updating Rooms', () => {
 
-            it('should update a specific record by id and verify its added to the database', (done) => {
-                let updateRoom = {
-                    room_name: 'Updated Room'
-                };
-                chai.request(app)
-                    .get('/api/rooms/')
-                    .end(function (err, res) {
-                        expect(res).to.have.status(200);
-                        chai.request(app)
-                            .put('/api/rooms/' + res.body[0]._id)
-                            .send(updateRoom)
-                            .end(function (error, response) {
-                                expect(res).to.have.status(200);
-                                done();
-                            });
-                    });
-            });
-            after(function (done) {
-                chai.request(app)
-                    .get('/api/rooms/')
-                    .end(function (err, res) {
-                        let result = _.map(res.body, (rooms) => {
-                            return {
-                                room_name: rooms.room_name,
-                            };
-                        });
-                        expect(res).to.have.status(200);
-                        expect(result).to.be.a('array');
-                        expect(result).to.include({room_name: 'Updated Room'});
-                        done();
-                    });
-            });
+            describe('Update Single Room by ID', () => {
 
-            it('should not update anything and get status 500 for incorrect id', function (done) {
-                let updateRoom = {
-                    room_name: 'Updated Room'
-                };
-                chai.request(app)
-                    .get('/api/rooms/')
-                    .end(function (err, res) {
-                        chai.request(app)
-                            .put('/api/rooms/12')
-                            .send(updateRoom)
-                            .end(function (err, res) {
-                                expect(res).to.have.status(500);
-                                done();
+                it('should update a specific record by id and verify its added to the database', (done) => {
+                    let updateRoom = {
+                        room_name: 'Updated Room'
+                    };
+                    chai.request(app)
+                        .get('/api/rooms/')
+                        .end(function (err, res) {
+                            expect(res).to.have.status(200);
+                            chai.request(app)
+                                .put('/api/rooms/' + res.body[0]._id)
+                                .send(updateRoom)
+                                .end(function (error, response) {
+                                    expect(res).to.have.status(200);
+                                    done();
+                                });
+                        });
+                });
+                after(function (done) {
+                    chai.request(app)
+                        .get('/api/rooms/')
+                        .end(function (err, res) {
+                            let result = _.map(res.body, (rooms) => {
+                                return {
+                                    room_name: rooms.room_name,
+                                };
                             });
-                    });
+                            expect(res).to.have.status(200);
+                            expect(result).to.be.a('array');
+                            expect(result).to.include({room_name: 'Updated Room'});
+                            done();
+                        });
+                });
+            });
+            describe('Error Handling', () => {
+
+                it('should not update anything and get status 500 for incorrect id', function (done) {
+                    let updateRoom = {
+                        room_name: 'Updated Room'
+                    };
+                    chai.request(app)
+                        .get('/api/rooms/')
+                        .end(function (err, res) {
+                            chai.request(app)
+                                .put('/api/rooms/12')
+                                .send(updateRoom)
+                                .end(function (err, res) {
+                                    expect(res).to.have.status(500);
+                                    done();
+                                });
+                        });
+                });
             });
 
         });
@@ -173,44 +186,51 @@ describe('Reviews', function () {
         aRoom.save()
         describe('Deleting Rooms', () => {
 
-            it('should delete room by id and remove the object instance', function (done) {
-                chai.request(app)
-                    .get('/api/rooms/')
-                    .end(function (err, res) {
-                        chai.request(app)
-                            .delete('/api/rooms/' + aRoom._id)
-                            .end(function (err, res) {
-                                expect(res).to.have.status(200);
-                                done();
-                            });
-                    });
-            });
-            after(function (done) {
-                chai.request(app)
-                    .get('/api/rooms/')
-                    .end(function (err, res) {
-                        expect(res).to.have.status(200);
-                        expect(res.body).be.be.a('array');
-                        let result = _.map(res.body, (rooms) => {
-                            return {
-                                room_name: rooms.room_name
-                            };
+
+            describe('Delete Single Room by ID', () => {
+
+                it('should delete room by id and remove the object instance', function (done) {
+                    chai.request(app)
+                        .get('/api/rooms/')
+                        .end(function (err, res) {
+                            chai.request(app)
+                                .delete('/api/rooms/' + aRoom._id)
+                                .end(function (err, res) {
+                                    expect(res).to.have.status(200);
+                                    done();
+                                });
                         });
-                        expect(result).to.not.include({room_name: 'Room delete-test'});
-                        done();
-                    })
-            })
-            it('should not delete anything and get status 500 for incorrect id', function (done) {
-                chai.request(app)
-                    .get('/api/rooms/')
-                    .end(function (err, res) {
-                        chai.request(app)
-                            .delete('/api/rooms/12')
-                            .end(function (err, res) {
-                                expect(res).to.have.status(500);
-                                done();
+                });
+                after(function (done) {
+                    chai.request(app)
+                        .get('/api/rooms/')
+                        .end(function (err, res) {
+                            expect(res).to.have.status(200);
+                            expect(res.body).be.be.a('array');
+                            let result = _.map(res.body, (rooms) => {
+                                return {
+                                    room_name: rooms.room_name
+                                };
                             });
-                    });
+                            expect(result).to.not.include({room_name: 'Room delete-test'});
+                            done();
+                        })
+                })
+            });
+            describe('Error Handling', () => {
+
+                it('should not delete anything and get status 500 for incorrect id', function (done) {
+                    chai.request(app)
+                        .get('/api/rooms/')
+                        .end(function (err, res) {
+                            chai.request(app)
+                                .delete('/api/rooms/12')
+                                .end(function (err, res) {
+                                    expect(res).to.have.status(500);
+                                    done();
+                                });
+                        });
+                });
             });
         });
 
